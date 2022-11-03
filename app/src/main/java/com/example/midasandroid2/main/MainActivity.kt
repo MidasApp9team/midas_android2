dc4ec5119de41b7213c8b2bbd1c5beb66381d7a8
kimjjunho <kimjjun1007@gmail.com>
2022-11-03 11:27:49 +0900

    feat: hilt

---
+++ app/src/main/java/com/example/midasandroid2/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.midasandroid2.main
+
+import com.example.midasandroid2.R
+import com.example.midasandroid2.base.BaseActivity
+import com.example.midasandroid2.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
+    override fun initView() {
+
+    }
+
+    override fun observeEvent() {}
+}
